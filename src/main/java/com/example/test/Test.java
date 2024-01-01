131b14834f748fd0d4b151d5291b4c418aaaf5eb
Hariyanto Djong <vtesha@hotmail.com>
2024-01-01 17:26:04 +0800

    feat(): testing

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -11,7 +11,7 @@ public class Test {
 
     @GetMapping("doh")
 	public String Doh() {
-		return "testing recent push this is doh";
+		return "testing babon recent push this is doh";
 	}
     
     @GetMapping("greeter")
